02714d5ce372d0132062b5a8cfadc6c45b8a3348
Sarah <sarah.baines@northcoders.com>
2020-11-02 11:15:36 +0000

    initial setup

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const { Client } = require("pg");
+const dbConfig = require("./config");
+
+const client = new Client(dbConfig);
+
+client
+  .connect()
+  .then(() => {
+    console.log(
+      `connected to the ${dbConfig.database} on port ${dbConfig.port}`
+    );
+  })
+  .catch((err) => {
+    console.log(`connection error:`, err);
+  });
+
+module.exports = client;
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS restaurants;
+CREATE DATABASE restaurants;
+
+\c restaurants;
